43cdadbaa4b85e8503e175026087197e4438988d
Jack Phoenix <ashley@uncyclomedia.co>
2020-04-30 04:40:27 +0300

    Modernize this thing a bit
    * Now uses the ParserFirstCallInit hook properly instead of $wgExtensionFunctions for registering the <metakeywords> tag with the parser
    * Main extension code moved to a separate file and wrapped inside a class
    * Docs adjusted as appropriate
    * Extension registration support
    * Long PHP arrays converted to short, as per coding conventions
    * Now requires MW 1.29+
    * Version number incremented from 0.3.0 to 0.4.0
    
    Change-Id: I275dc3cd8bd611db73dc7347bdf719f0643c1bfa
    (cherry picked from commit d3746f29f06534f0fb10cc02d9b0b048d3ec8d39)

--- YetAnotherKeywords.php
+++ YetAnotherKeywords.php
@@ -9,19 +9,9 @@
  * Description:
  *     This is a MediaWiki extension which adds support for injecting a <meta> keywords tag
  *     into the page header.
- * Requirements:
- *     MediaWiki 1.6.x, 1.8.x, 1.9.x, ... , 1.19 or higher
- *     PHP 4.x, 5.x or higher
-  * Installation:
- *     1. Drop this script (YetAnotherKeywords.php) in $IP/extensions
- *         Note: $IP is your MediaWiki install dir.
- *     2. Enable the extension by adding this line to your LocalSettings.php:
- *         require_once('extensions/YetAnotherKeywords.php');
  * Usage:
  *     Once installed, you may utilize YetAnotherKeywords by adding the <metakeywords> tag to articles:
  *         <metakeywords> Home page for the YetAnotherKeywords MediaWiki extension </metakeywords>
- * Version Notes:
- *     version 0.2
  * -----------------------------------------------------------------------
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -45,82 +35,16 @@
  * -----------------------------------------------------------------------
  */
 
-if( !defined( 'MEDIAWIKI' ) ) {
-	echo "This file is part of MediaWiki, it is not a valid entry point.\n";
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'YetAnotherKeywords' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$GLOBALS['wgMessagesDirs']['YetAnotherKeywords'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for YetAnotherKeywords extension.' .
+		'Please use wfLoadExtension instead,' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the YetAnotherKeywords extension requires MediaWiki 1.29+' );
 }
-
-# Credits
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'YetAnotherKeywords',
-	'author' => 'Jehy http://jehy.ru/index.en.html',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:YetAnotherKeywords',
-	'descriptionmsg' => 'yetanotherkeywords-desc',
-	'version'=> '0.3.0'
-);
-
-$wgMessagesDirs['YetAnotherKeywords'] = __DIR__ . '/i18n';
-
-$wgExtensionFunctions[] = 'setupYetAnotherKeywordsParserHooks';
-
-/**
- * Sets up the YetAnotherKeywords Parser hook and system messages
- */
-function setupYetAnotherKeywordsParserHooks() {
-	global $wgParser, $wgMessageCache;
-	$wgParser->setHook( 'metakeywords', 'renderYetAnotherKeywords' );
-	/*$wgMessageCache->addMessage(
-		'YetAnotherKeywords-missing-content',
-		'Error: &lt;metakeywn&gt; tag must contain a &quot;content&quot; attribute.'
-	);*/
-}
-
-/**
- * Renders the <metakeywords> tag.
- * @param String $text Incoming text - should always be null or empty (passed by value).
- * @param Array $params Attributes specified for tag - must contain 'content' (passed by value).
- * @param Parser $parser Reference to currently running parser (passed by reference).
- * @return String Always empty.
- */
-function renderYetAnotherKeywords( $text, $params = array(), $parser ) {
-
-	// Short-circuit with error message if content is not specified.
-	if ( !isset($text) ) {
-		return
-			'<div class="errorbox"&gt;'.
-			wfMessage('YetAnotherKeywords-missing-content')->inContentLanguage()->escaped().
-			'</div&gt;';
-	}
-
-	return '<!-- META_KEYWORDS '.base64_encode($text).' -->';
-}
-
-// Attach post-parser hook to extract metadata and alter headers
-$wgHooks['OutputPageBeforeHTML'][] = 'insertMetaKeywords';
-
-/**
- * Adds the <meta> keywords to document head.
- * Usage: $wgHooks['OutputPageBeforeHTML'][] = 'insertMetaKeywords';
- * @param OutputPage $out Handle to an OutputPage object - presumably $wgOut (passed by reference).
- * @param String $text Output text.
- * @return Boolean Always true to allow other extensions to continue processing.
- */
-function insertMetaKeywords( $out, $text ) {
-
-	// Extract meta keywords
-	if (preg_match_all(
-		'/<!-- META_KEYWORDS ([0-9a-zA-Z\\+\\/]+=*) -->/m',
-		$text,
-		$matches)===false
-	) return true;
-	$data = $matches[1];
-	// Merge keywords data into OutputPage as meta tag
-	foreach ($data AS $item) {
-		$content = @base64_decode($item);
-		if ($content)
-			$out->addMeta( 'keywords', $content );
-	}
-	return true;
-}
-?>
